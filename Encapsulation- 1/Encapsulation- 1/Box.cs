a1160a0ec9160747dca677814dd908832308afb7
Steliyan003 <steliyan2rumenov@gmail.com>
2025-08-01 14:58:42 +0300

    .

---
+++ Encapsulation- 1/Encapsulation- 1/Box.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation__1
+{
+    public class Box
+    {
+        private double Length { get; set; }
+        private double Width { get; set; }
+        private double Height { get; set; }
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+
+            if (length <= 0 )
+            { 
+                throw new ArgumentException("Length cannot be zero or negative.");
+            }
+            if (width <= 0)
+            {
+                throw new ArgumentException("Width cannot be zero or negative.");
+            }
+            else if (height <= 0)
+            {
+                throw new ArgumentException("Height cannot be zero or negative.");
+            }
+
+            double surfaceArea= 2*(length * width + length * height + width * height);
+            double lateralSurfaceArea = 2 * (length * height + width * height);
+            double volume = length * width * height;
+
+            Console.WriteLine($"Surface Area - {surfaceArea:f2}");
+            Console.WriteLine($"Lateral Surface Area - {lateralSurfaceArea:f2}");
+            Console.WriteLine($"Volume - {volume:f2}");
+        }
+    }
+}
