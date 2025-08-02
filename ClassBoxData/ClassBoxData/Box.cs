972a734f54bbb52dde81e5dd97870ea43ea0217a
Steliyan003 <steliyan2rumenov@gmail.com>
2025-08-02 23:22:49 +0300

    .

---
+++ ClassBoxData/ClassBoxData/Box.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+           this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+            private set
+            {
+               if(value<=0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            private set
+            {
+                if(value<=0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                this.width = value;
+            }
+        }
+
+        public double Height 
+        {
+            get
+            {
+                return height;
+            }
+            private set
+            {
+                if (value<=0)
+                {
+                  throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                this.height = value;
+            }
+
+        }       
+
+        public double SurfaceArea(double length, double width, double height)
+        {
+            return (2*length*width)+(2* length * height) + (2 * width * height);
+        }
+
+        public double LateralSurfaceArea(double length, double width, double height)
+        {
+            return (2 * length * height) + (2 * width * height);
+        }
+
+        public double Volume(double length, double width, double height)
+        {
+            return length * width * height;
+        }
+    }
+}
---
+++ ClassBoxData/ClassBoxData/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ClassBoxData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Write the length");
+                double length = double.Parse(Console.ReadLine());
+                Console.WriteLine("Write the width");
+                double width = double.Parse(Console.ReadLine());
+                Console.WriteLine("Write the height");
+                double height = double.Parse(Console.ReadLine());
+               
+
+                Box box = new Box(length, width, height);
+                Console.WriteLine($"Surface Area - {box.SurfaceArea(width, height, length):f2} ");
+                Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea(width, height, length):f2}");
+                Console.WriteLine($"Volume - {box.Volume(width, height, length):f2}");
+            }
+
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
