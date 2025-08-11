14246e8980a44f24d4ee411efec84c10823c75aa
Steliyan003 <steliyan2rumenov@gmail.com>
2025-08-11 15:31:37 +0300

    .

---
+++ ExplicitInterface/ExplicitInterface/Citizen.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplicitInterface
+{
+    public class Citizen : IResident, IPerson
+    {
+       private string name ;
+        
+        private string country;
+        private int age;
+
+        public Citizen(string name, string country, int age)
+        {
+            this.Name = name;
+            this.Country = country;
+            this.Age = age;
+            
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be null or empty.");
+                }
+                name = value;
+            }
+        }
+
+        public string Country
+        {
+            get => country;
+           private set
+            {
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Country cannot be null or empty.");
+                }
+                country = value;
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age cannot be negative.");
+                }
+                age = value;
+            }
+        }
+
+        string IResident.GetName()
+        {
+            return "Mr/Ms/Mrs";
+        }
+
+        string IPerson.GetName()
+        {
+            return this.name;
+        }
+    }
+}
---
+++ ExplicitInterface/ExplicitInterface/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.Metrics;
+using System.Xml.Linq;
+
+namespace ExplicitInterface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] smd = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+            List<Citizen> citizens = new List<Citizen>();
+
+            while (!smd.Contains("End"))
+            {
+                if (smd.Length == 3)
+                {
+                    string name = smd[0];
+                    string country = smd[1];
+                    int age = int.Parse(smd[2]);
+ 
+                    Citizen citizen = new Citizen(name, country, age);
+                    citizens.Add(citizen);
+                }
+
+                smd = Console.ReadLine()
+                            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                            .ToArray();
+            }
+            foreach(var citizen in citizens)
+            {
+                IResident resident = citizen;
+                IPerson person = citizen;
+                Console.WriteLine(person.GetName());
+                Console.WriteLine($"{resident.GetName()} {person.GetName()}");
+            }
+
+        }
+    }
+}
